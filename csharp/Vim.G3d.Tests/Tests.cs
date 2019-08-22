c5d2222debbc337a1d4edf1bd267513a69851266
cdiggins <christopher.diggins@vimaec.com>
2019-08-22 10:36:28 -0400

    Assuring the unity adapter gets pushed to the UNity projecbuilt to the unity project sub-folder

--- csharp/Vim.G3d.Tests/Tests.cs
+++ csharp/Vim.G3d.Tests/Tests.cs
@@ -1,29 +1,25 @@
 ﻿using NUnit.Framework;
 using System;
 using System.IO;
+using System.Reflection;
 
 namespace Vim.G3d.Tests
 {
     public static class Tests
     {
-        public static string InputDataPath = Path.Combine(TestContext.CurrentContext.TestDirectory,
-            "..", "..", "..", "..", "..",
+        public static string InputDataPath => Path.Combine(TestContext.CurrentContext.TestDirectory,
+            "..", "..", "..", "..", "..", // yes 5, count em, 5  
             "data", "assimp", "test");
 
         [Test]
         public static void TestAssimp()
         {
+            Console.WriteLine(InputDataPath);
             var file = Path.Combine(InputDataPath, "models", "STL", "wuson.stl");
             using (var context = new Assimp.AssimpContext())
             {
-                context.ImportFile(file);
+                var scene = context.ImportFile(file);
             }
-        }
-
-        public static void Main()
-        {
-            TestAssimp();
-            Console.WriteLine("Test");
         }
     }
 }
